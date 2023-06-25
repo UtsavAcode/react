f94e8ddafa3de03a1b89ea6bc9803d6219f4a9d9
utsav <utsavadhikari452@gmail.com>
2023-06-25 13:23:47 +0545

    man its not working

--- src/index.js
+++ src/index.js
@@ -1,78 +1,115 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDom from "react-dom/client";
 import "./css/index.css";
-// const Greeting = () => {
-//   return React.createElement(
-//     "div",
-//     {},
-//     React.createElement("h1", {}, "Hello again")
-//   );
-// };
 
-// This is the component that we actually want to render.
+// I need to create a Booklist of the most famous books and mangas.
 
-const Booklist = () => {
+const firstBook = {
+  title: "Harry Potter one",
+  author: "J.K. Rowling",
+  img: "./images/one.jpg",
+};
+
+const secondBook = {
+  title: "One Piece",
+  author: "Eichiro Oda",
+  img: "./images/one.jpg",
+};
+
+const BookList = () => {
   return (
-    <section className="booklist">
-      <Book
-        author={firstBook.author}
-        title={firstBook.title}
-        img={firstBook.img}
-      />
+    <div>
+      <section className="booklist">
+        <Book
+          title={firstBook.title}
+          author={firstBook.author}
+          img={firstBook.img}
+        />
+        <Book
+          title={secondBook.title}
+          author={secondBook.author}
+          img={secondBook.img}
+        />
+        <Book
+          title={secondBook.title}
+          author={secondBook.author}
+          img={secondBook.img}
+        />
+      </section>
 
-      <Book
-        author={secondBook.author}
-        title={secondBook.tile}
-        img={secondBook.img}
-      />
+      <div className="but">
+        <Click />
+      </div>
 
-      <Book
-        author={thirdBook.author}
-        title={thirdBook.title}
-        img={thirdBook.img}
-      />
-    </section>
+      {/* subject section */}
+
+      <div className="subjects">
+        <Subject
+          title={firstSubject.title1}
+          author={firstSubject.author1}
+          image={firstSubject.image1}
+        />
+
+        <Subject
+          title={secondSubject.title1}
+          author={secondSubject.author1}
+          image={secondSubject.image1}
+        />
+        <Subject
+          title={thirdSubject.title1}
+          author={thirdSubject.author1}
+          image={thirdSubject.image1}
+        />
+      </div>
+    </div>
   );
 };
 
-// This Book component is going to be rendered inside the Booklist component.
-
-// store all the details of the books here.
-
-//Details of the first book.
-const firstBook = {
-  author: "J.K. rowling",
-  title: "Harry Potter and The Sorcerr's Stone",
-  img: "https://m.media-amazon.com/images/I/51HSkTKlauL._SX346_BO1,204,203,200_.jpg",
-};
-
-//Details of the second book.
-
-const secondBook = {
-  author: "Eichiro Oda",
-  title: "One Piece",
-  img: "./images/three.jpg",
-};
-
-//Details of the third book.
-
-const thirdBook = {
-  author: "Gege Akutami",
-  title: "Jujutsu Kaisen",
-  img: "./images/two.jpg",
-};
-
 const Book = (props) => {
+  const { title, author, img } = props;
   return (
     <article className="book">
-      <img src={props.img} alt={props.title} />
-      <h2>{props.title}</h2>
-      <h4>{props.author}</h4>
+      <img src={img} alt={title} />
+      <h2>{title}</h2>
+      <h4>{author}</h4>
     </article>
   );
 };
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const Click = () => {
+  return <button type="button">Click me</button>;
+};
 
-// root.render(<Greeting />);
-root.render(<Booklist />);
+// All the details of the ssubject.
+const firstSubject = {
+  title1: "React JS",
+  author1: "Facebook",
+  image1: "./images/react.png",
+};
+
+const secondSubject = {
+  title1: "Java",
+  author1: "Oracle",
+  image1: "./images/java.png",
+};
+
+const thirdSubject = {
+  title1: "Pyhton",
+  author1: "Guido van Rossum",
+  image1: "./images/pyton.png",
+};
+
+const Subject = (props) => {
+  const { image1, title1, author1 } = props;
+  return (
+    <article className="sub">
+      <img src={image1} alt={title1} />
+      <h2>{title1}</h2>
+      <h4>{author1}</h4>
+    </article>
+  );
+};
+
+const root = ReactDom.createRoot(document.getElementById("root"));
+
+root.render(<BookList />);
